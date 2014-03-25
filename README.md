2848993f363a542b6821b6eed38d866a9f0134e0
marcusmoller <marcus@powrtoch.org>
2014-03-25 18:23:50 +0100

    Added DR Bonanza support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 drtv-dl
 =======
 
-Small command-line program to download videos from DR TV (www.dr.dk/tv/)
+Small command-line program to download videos from DR TV (www.dr.dk/tv/) or DR Bonanza (www.dr.dk/bonanza/)
 
 AUR package available here: https://aur.archlinux.org/packages/drtv-dl-git/
--- drtv-dl.py
+++ drtv-dl.py
@@ -10,6 +10,7 @@ except ImportError:
 import sys
 import json
 import argparse
+from bs4 import BeautifulSoup
 
 def parseString(data, findStart, findEnd):
     ''' returns string between findStart and findEnd '''
@@ -43,7 +44,7 @@ def _reportHook(count, block_size, total_size):
     sys.stdout.flush()
 
 def downloadDRTV(url, output=None):
-    print('Finding json data URL...')
+    print('Finding json data URL from DR TV...')
     # find json data
     siteContent = getUrlContent(url)
     jsonUrl = parseString(siteContent, 'resource: "', '"')
@@ -99,15 +100,103 @@ def downloadDRTV(url, output=None):
     # download the film
     if output is None:
         urllib.urlretrieve(streamUrl, filmData['title'] + '.mp4', reporthook=_reportHook)
+        print('')
     else:
         urllib.urlretrieve(streamUrl, output, reporthook=_reportHook)
+        print('')
 
+    print('Download finished! Quitting')
+
+def downloadDRBonanza(url, output=None):
+    print('Finding video files from DR Bonanza...')
+
+    # fetch html content
+    site_content = getUrlContent(url)
+
+    # initialize beautifulsoup
+    html_soup = BeautifulSoup(site_content)
+
+    # parse the html into json
+    programs = []
+    for link in html_soup.find_all(class_='listItem Video'):
+        program = {}
+        # parse onclick event from links
+        onclick_string = link['onclick']
+
+        # fetch json
+        i = onclick_string.find('{')
+        json_string = onclick_string[i:-2]
+        json_obj = json.loads(json_string)
+
+        # fetch program information
+        program['id'] = link['id']
+        program['title'] = json_obj['Title']
+        program['description'] = json_obj['Description']
+
+        # find highest quality
+        for file_obj in json_obj['Files']:
+            if 'VideoHigh' in file_obj['Type']:
+                # highest video stream found, generate download link
+                video_link = 'http://vodfiles.dr.dk/' + file_obj['Location'].split(':')[-1]
+                program['video'] = video_link
+
+        programs.append(program)
+
+    # get user input
+    print('There are ' + str(len(programs)) + ' programs available for download:')
+    i = 1 # start from 1 as 0 == ALL
+    for program in programs:
+        print(str(i) + ') ' + program['title'])
+        i += 1
+
+    # python2 input support
+    try:
+        get_input = raw_input
+    except NameError:
+        get_input = input
+
+    # get user input for download
+    input_waiting = True
+    while input_waiting:
+        print('')
+        cmd = get_input('Enter a selection (default=all): ')
+        if cmd == '':
+            # default=all
+            input_waiting = False
+        elif 0 < int(cmd) < len(programs)+1:
+            input_waiting = False
+        else:
+            print('error: invalid value: ' + str(cmd) + ' is not between 1 and ' + str(len(programs)))
+
+    # parse user input
+    if cmd == '':
+        print('You chose to download all listed programs!')
+
+        # loop through programs and download each one 
+        for program in programs:
+            print('Starting download of: "' + program['title'] + '"...')
+            urllib.urlretrieve(program['video'], program['title'] + '.mp4', reporthook=_reportHook)
+            print('')
+
+    else:
+        desired_program = programs[int(cmd)-1]
+        print('You selected: "' + desired_program['title'] + '"')
+        
+        # download the program
+        if output is None:
+            urllib.urlretrieve(desired_program['video'], desired_program['title'] + '.mp4', reporthook=_reportHook)
+            print('')
+        else:
+            urllib.urlretrieve(desired_program['video'], output, reporthook=_reportHook)
+            print('')
+
+    # assume that download has finished
     print('Download finished! Quitting')
 
 if __name__ == '__main__':
     # init parser
-    parser = argparse.ArgumentParser(prog='drtv-dl', description='Small command-line program to download videos from DR TV (www.dr.dk/tv/)')
-    parser.add_argument('URL', nargs='+', help='The URL of the DR TV stream')
+    parser = argparse.ArgumentParser(prog='drtv-dl', description='Small command-line program to download videos from DR TV (www.dr.dk/tv/) and DR Bonanza (www.dr.dk/bonanza/)')
+    parser.add_argument('URL', nargs='+', help='The URL of the DR TV or DR Bonanza stream')
     parser.add_argument('-o', help='Output file')
 
     args = vars(parser.parse_args())
@@ -117,4 +206,10 @@ if __name__ == '__main__':
     outputFile = args['o']
 
     # start application
-    downloadDRTV(url, outputFile)
+    if 'bonanza' in url.lower():
+        downloadDRBonanza(url, outputFile)
+    elif 'tv' in url.lower():
+        downloadDRTV(url, outputFile)
+    else:
+        print('Error occured. URL is not valid!')
+
